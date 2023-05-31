8d88928b4e7d63ec6ae405ca384b793e5c956556
HomieKim <ghals0708@gmail.com>
2023-05-31 23:49:07 +0900

    fix: data filtering

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,11 +3,21 @@ import { rest } from 'msw'
 export const handlers = [
   rest.get('/list', (_, res, ctx) => {
     const list = sessionStorage.getItem('item')
-    const item = JSON.parse(list!)
+    const items = (JSON.parse(list as string) as Array<Product>).map(item => {
+      const { id, name, price, imageUrl, isSoldOut } = item
+      return {
+        id,
+        name,
+        price,
+        imageUrl,
+        isSoldOut,
+      }
+    })
+
     return res(
       ctx.status(200),
       ctx.json({
-        item,
+        list: items,
       }),
     )
   }),
